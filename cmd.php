c4e309b616eb2370ecb0cca29a455a9c72db5441
André <andregds85@gmail.com>
2022-09-11 17:52:29 -0300

    Buscando dados da API

---
+++ cmd.php
@@ -0,0 +1,34 @@
+video base 
+https://www.youtube.com/watch?v=p-xkYfY2Xo0&t=373s
+
+https://cli.vuejs.org/
+
+Instalando vue Cli 
+npm install vue@next
+npm install -g @vue/cli
+
+Verificando a versão do vue 
+vue --version 
+
+Criar um projeto com Vue 
+vue create vuecad
+
+Rodar o servidor 
+npm run serve 
+
+Adicionar as Routes
+vue add router 
+
+Dentro da Pasta commponente 
+vamos criar 
+
+Cadastrar.vue
+listar.vue 
+Editar.vue
+
+
+
+
+
+
+parei em 14:30
